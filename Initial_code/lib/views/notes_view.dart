01362d2bda3a7da655152250048557e0e2499050
Adarshkumar167 <adarshkumar167@gmail.com>
2024-01-22 11:56:17 +0530

    28

--- Initial_code/lib/views/notes_view.dart
+++ Initial_code/lib/views/notes_view.dart
@@ -40,7 +40,7 @@ class _NotesViewState extends State<NotesView> {
           })
         ],
       ),
-      body: const Text('Hellow world'),
+      body: const Text('Hello world'),
     );
   }
 }
