ed84688470640e07cd9894d601e882b1e37274eb
Adarshkumar167 <adarshkumar167@gmail.com>
2024-01-22 18:17:03 +0530

    35

--- Initial_code/lib/services/auth/crud/crud_exception.dart
+++ Initial_code/lib/services/auth/crud/crud_exception.dart
@@ -15,3 +15,5 @@ class CouldNotDeleteNote implements Exception {}
 class CouldNotFindNote implements Exception {}
 
 class CouldNotUpdateNotes implements Exception {}
+
+class UserShouldBeSetBeforeReadingAllNotes implements Exception {}
